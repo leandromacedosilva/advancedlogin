c01bb46bf403b5f84b0de983267b2f9330ca5aec
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-03-02 14:53:04 -0300

    added directory assets/ in project

---
+++ assets/js/file_js_one.js
@@ -0,0 +1,4 @@
+let nome = window.prompt('Informe seu nome');
+let nota1 = window.prompt('Inofrme um valor para a primeira nota');
+window.alert('O nome informado foi = ' + nome);
+window.alert('O valor da nota informado foi = ' + nota1);
