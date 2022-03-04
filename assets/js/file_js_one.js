f4490efc462b6b15be77220a75bf81606f05979e
leandromacedosilva <iamfulldeveloper@gmail.com>
2022-03-04 19:44:09 -0300

    deleted file file_js_one.js

--- assets/js/file_js_one.js
+++
@@ -1,4 +0,0 @@
-let nome = window.prompt('Informe seu nome');
-let nota1 = window.prompt('Inofrme um valor para a primeira nota');
-window.alert('O nome informado foi = ' + nome);
-window.alert('O valor da nota informado foi = ' + nota1);
